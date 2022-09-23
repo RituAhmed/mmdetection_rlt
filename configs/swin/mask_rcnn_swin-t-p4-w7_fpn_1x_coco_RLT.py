af0a1195fd12586d900a66782894a5f36758863f
RituAhmed <ritukucse12@gmail.com>
2022-09-23 14:25:27 +0200

    including training for retinanet_swin_t

--- configs/swin/mask_rcnn_swin-t-p4-w7_fpn_1x_coco_RLT.py
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_1x_coco_RLT.py
@@ -1,10 +1,11 @@
 _base_ = [
-    '../_base_/models/retinanet_r50_fpn.py',
-    '../_base_/datasets/coco_detection.py',
+    '../_base_/models/mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/RLT_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
 model = dict(
+    type='MaskRCNN',
     backbone=dict(
         _delete_=True,
         type='SwinTransformer',
@@ -19,12 +20,23 @@ model = dict(
         attn_drop_rate=0.,
         drop_path_rate=0.2,
         patch_norm=True,
-        out_indices=(1, 2, 3),
-        # Please only add indices that would be used
-        # in FPN, otherwise some parameter will not be used
+        out_indices=(0, 1, 2, 3),
         with_cp=False,
         convert_weights=True,
         init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
-    neck=dict(in_channels=[192, 384, 768], start_level=0, num_outs=5))
+    neck=dict(in_channels=[96, 192, 384, 768]))
 
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+lr_config = dict(warmup_iters=1000, step=[8, 11])
+runner = dict(max_epochs=12)
